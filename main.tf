e75724493ecbd84ed806ab9beea0ffc74a6e46ca
Preetam <ec2-user@ip-172-31-79-194.ec2.internal>
2023-04-12 13:08:18 +0000

    wed

--- main.tf
+++ main.tf
@@ -10,6 +10,7 @@ resource "aws_docdb_cluster" "main" {
   db_subnet_group_name    = aws_docdb_subnet_group.main.name
   kms_key_id              = data.aws_kms_key.key.arn
   storage_encrypted       = var.storage_encrypted
+  vpc_security_group_ids  = [aws_security_group.main.id]
 }
 
 resource "aws_docdb_cluster_instance" "cluster_instances" {
@@ -17,6 +18,34 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   identifier         = "${var.env}-docdb-${count.index}"
   cluster_identifier = aws_docdb_cluster.main.id
   instance_class     = var.instance_class
+}
+
+resource "aws_security_group" "main" {
+  name        = "docdb-${var.env}"
+  description = "docdb-${var.env}"
+  vpc_id      = var.vpc_id
+  
+  ingress {
+    description      = "DOCDB"
+    from_port        = 27017
+    to_port          = 27017
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_subnets
+    
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ tags   = merge(
+   var.tags,
+  { Name = "${var.name}-${var.env}-lb" }
+   )
 }
 
 resource "aws_docdb_subnet_group" "main" {  
@@ -30,5 +59,22 @@ resource "aws_docdb_subnet_group" "main" {
    
   }
 
+resource "aws_ssm_parameter" "docdb_url_catalogue" {
+  name  = "${var.env}.docdb.url.catalogue"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@${aws_docdb_cluster.main.endpoint}:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_url_user" {
+  name  = "${var.env}.docdb.url.user"
+  type  = "String"
+  value = "mongodb://${data.aws_ssm_parameter.user.value}:${data.aws_ssm_parameter.pass.value}@${aws_docdb_cluster.main.endpoint}:27017/users?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false"
+}
+
+resource "aws_ssm_parameter" "docdb_endpoint" {
+  name  = "${var.env}.docdb.endpoint"
+  type  = "String"
+  value = aws_docdb_cluster.main.endpoint
+}
 
 
